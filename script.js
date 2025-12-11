d9e9887e06da1af631fa6ee15265398089b144fd
elmkalizineb <elmkalizineb@gmail.com>
2025-12-11 20:53:31 +0100

    styling done

---
+++ script.js
@@ -0,0 +1,14 @@
+const taskInput = document.getElementById("task-input");
+const addBtn = document.getElementById("add-btn");
+const taskList = document.getElementById("task-list");
+// add an event to the add button 
+addBtn.addEventListener("click", function () {
+    const taskText = taskInput.value.trim();
+    // check if the iput is 
+    if (taskText === "") {
+        return; // don’t add empty tasks
+    }
+
+    
+    
+});
