ad64423ae5bce65da461fb93573c119f48d7c033
elmkalizineb <elmkalizineb@gmail.com>
2025-12-20 17:42:38 +0100

    add a task

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
+// Variables 
+const addTaskBtn = document.getElementById('add-btn');
 const taskInput = document.getElementById("task-input");
-const addBtn = document.getElementById("add-btn");
-const taskList = document.getElementById("task-list");
-// add an event to the add button 
-addBtn.addEventListener("click", function () {
+// Add Task Event 
+addTaskBtn.addEventListener("click", () => {
     const taskText = taskInput.value.trim();
-    // check if the iput is 
-    if (taskText === "") {
-        return; // don’t add empty tasks
+
+
+    if (taskText === '') {
+        return; // we don't add empty tasks 
     }
 
-    
-    
+    // add the task  on the TO-DO list 
+    const node = document.createElement("li");
+    node.innerHTML =`
+                <div class="p-check-task">
+                  <label class="circle-checkbox">
+                    <input type="checkbox" />
+                    <span class="checkmark"></span>
+                  </label>
+                  <p>${taskText}</p>
+                </div>
+                <button type="submit" id="delete-btn">
+                  <img src="images/x.png" alt="delete" />
+                </button>
+    `;
+    document.getElementById("task-list").appendChild(node);
+    // clear input 
+    taskInput.value = "";
 });
+
+// delete a task 
+
